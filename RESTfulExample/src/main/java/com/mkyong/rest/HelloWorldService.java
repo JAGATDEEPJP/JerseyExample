327ccee7f7bb4739a8c957bfd2149a83084a4c0d
KavyaKPuranik <kpuranik08@gmail.com>
2018-10-12 16:40:50 +0530

    Quadratic Equation

--- RESTfulExample/src/main/java/com/mkyong/rest/HelloWorldService.java
+++ RESTfulExample/src/main/java/com/mkyong/rest/HelloWorldService.java
@@ -4,18 +4,24 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
- 
+
 @Path("/hello")
-public class HelloWorldService {
- 
+public class HelloWorldService 
+{
+	
+	@GET
+	public Response getMsg() 
+	{
+		String output = "Jersey says : Hello World" ;
+		return Response.status(200).entity(output).build(); 
+	} 
+	
 	@GET
 	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
- 
-		String output = "Jersey say : " + msg;
- 
-		return Response.status(200).entity(output).build();
- 
-	}
- 
+	public Response getName(@PathParam("param") String name) 
+	{
+		String output = "Jersey says : Hello " + name; 
+		return Response.status(200).entity(output).build(); 
+	} 
+
 }
