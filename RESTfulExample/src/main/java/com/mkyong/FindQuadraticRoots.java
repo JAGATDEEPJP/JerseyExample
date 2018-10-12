9a5cfafdef5032367714bb1f3b518bf8f14e3faa
KavyaKPuranik <kpuranik08@gmail.com>
2018-10-12 23:26:48 +0530

    Full working of sample

--- RESTfulExample/src/main/java/com/mkyong/FindQuadraticRoots.java
+++ RESTfulExample/src/main/java/com/mkyong/FindQuadraticRoots.java
@@ -6,7 +6,9 @@ public class FindQuadraticRoots
 
 	public FindQuadraticRoots(double a, double b, double c)
 	{
+
 		double r1, r2;
+
 		double determinant = b * b - 4 * a * c;
 
 		// condition for real and different roots
@@ -14,28 +16,29 @@ public class FindQuadraticRoots
 		{
 			r1 = (-b + Math.sqrt(determinant)) / (2 * a);
 			r2 = (-b - Math.sqrt(determinant)) / (2 * a);
+
 			root1 = String.format("%.2f", r1);
 			root2 = String.format("%.2f", r2);
-			System.out.format("root1 = %.2f and root2 = %.2f", root1, root2);
+			System.out.println("r1 = " + root1 + " and r2 = " + root2);
 		}
 		// Condition for real and equal roots
 		else if (determinant == 0)
 		{
 			r1 = r2 = -b / (2 * a);
+
 			root1 = String.format("%.2f", r1);
-			root2 = String.format("%.2f", r1);
-			System.out.format("root1 = root2 = %.2f;", root1);
+			root2 = String.format("%.2f", r2);
+			System.out.println("r1 = " + root1 + " and r2 = " + root2);
 		}
 		// If roots are not real
 		else
 		{
 			double realPart = -b / (2 * a);
 			double imaginaryPart = Math.sqrt(-determinant) / (2 * a);
-			root1 = String.format("%.2f", realPart) + " + " + String.format("%.2f", imaginaryPart) +"i";
-			root2 = String.format("%.2f", realPart) + " - " + String.format("%.2f", imaginaryPart) +"i";
 
-			System.out.format("root1 = %.2f+%.2fi and root2 = %.2f-%.2fi", realPart, imaginaryPart, realPart,
-					imaginaryPart);
+			root1 = String.format("%.2f", realPart) + " + " + String.format("%.2f", imaginaryPart) + "i";
+			root2 = String.format("%.2f", realPart) + " - " + String.format("%.2f", imaginaryPart) + "i";
+			System.out.println("r1 = " + root1 + " and r2 = " + root2);
 		}
 	}
 }
